5d8778e4da33faaf84f4b8fb6813dc7fd75b6400
samueljoly <samueljoly0@gmail.com>
2023-11-13 00:48:39 +0100

    Mysql removed native_password auth (Issure #1)
    replace the mysql_native_password value by sha2_cache

--- env.dev.sh
+++ env.dev.sh
@@ -3,7 +3,8 @@ DB_MIGRATION="./docker/mysql/migrations"
 DB_USER="${PROJECT}_user"
 DB_PASSWORD="${PROJECT}_password"
 DB_NAME="$PROJECT"
+DB_URL="docker-db"
 
-env_list=("PROJECT" "DB_MIGRATION" "DB_USER" "DB_PASSWORD" "DB_NAME")
+env_list=("PROJECT" "DB_MIGRATION" "DB_USER" "DB_PASSWORD" "DB_NAME" "DB_URL")
 
 
