ec8f64d2a209928a1900b97b5225b8b639400b38
lthullier <l.thullier@otaree.com>
2023-11-17 10:38:33 +0100

    Declare variables when keywords are missing, use const keyword as much as possible, check isNaN correcly and remove unused event callback param

--- src/public/form-events.js
+++ src/public/form-events.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", (e) => {
+document.addEventListener("DOMContentLoaded", () => {
     form_step();
     form_step_click();
     form_2();
@@ -8,9 +8,9 @@ document.addEventListener("DOMContentLoaded", (e) => {
 
 
 function form_step() {
-    let next_btn = document.querySelector("#continue-form");
-    let prev_btn = document.querySelector("#prev-form");
-    let count = 1;
+    const next_btn = document.querySelector("#continue-form");
+    const prev_btn = document.querySelector("#prev-form");
+    const count = 1;
 
     if (count == 1) {
         prev_btn.classList.add("opacity-0");
@@ -120,22 +120,22 @@ function form_2() {
 
 
 function form_date() {
-    let start_date = document.querySelector("input[name=\"start-date\"]");
-    let end_date = document.querySelector("input[name=\"end-date\"]");
+    const start_date = document.querySelector("input[name=\"start-date\"]");
+    const end_date = document.querySelector("input[name=\"end-date\"]");
     start_date.setAttribute("min", new Date().toISOString().split("T")[0]);
     start_date.value = new Date().toISOString();
 
     start_date.addEventListener("change", () => {
-        let start_date = document.querySelector("input[name=\"start-date\"]");
-        let end_date = document.querySelector("input[name=\"end-date\"]");
+        const start_date = document.querySelector("input[name=\"start-date\"]");
+        const end_date = document.querySelector("input[name=\"end-date\"]");
 
         if (end_date.value == "") {
             end_date.setAttribute("min", start_date.value);
             end_date.value = start_date.value;
         }
 
-        let total_time = ((new Date(end_date.value) - new Date(start_date.value)) / (3600 * 24)) / 1000;
-        if (total_time != NaN) {
+        const total_time = ((new Date(end_date.value) - new Date(start_date.value)) / (3600 * 24)) / 1000;
+        if (!isNaN(total_time)) {
             show_reserved_days(total_time, start_date);
         } else {
             end_date.setAttribute("min", start_date.value);
@@ -144,22 +144,23 @@ function form_date() {
     });
 
     end_date.addEventListener("change", () => {
-        let start_date = document.querySelector("input[name=\"start-date\"]");
-        let end_date = document.querySelector("input[name=\"end-date\"]");
+        const start_date = document.querySelector("input[name=\"start-date\"]");
+        const end_date = document.querySelector("input[name=\"end-date\"]");
         /* document.querySelector("#form-step-2").appendChild(time_table); */
-        let total_time = ((new Date(end_date.value) - new Date(start_date.value)) / (3600 * 24)) / 1000;
+        const total_time = ((new Date(end_date.value) - new Date(start_date.value)) / (3600 * 24)) / 1000;
         show_reserved_days(total_time, start_date);
     });
 }
 
 function show_reserved_days(total_time, start_date) {
-    i = 0;
-    j = 4;
+    let i = 0;
+    let j = 4;
     while (total_time < j) {
         document.querySelector("#day-" + j + "-row").classList.add("opacity-0");
         j--;
     }
 
+    let day;
     while (i < total_time + 1) {
         switch (parseInt(new Date(start_date.value).getDay()) + i) {
             case 0:
@@ -186,11 +187,11 @@ function show_reserved_days(total_time, start_date) {
         }
 
         if (day != "WE") {
-            let date = new Date(start_date.value).getDate() + i + "/" + (parseInt(new Date(start_date.value).getMonth()) + 1) + "/2023";
+            const date = new Date(start_date.value).getDate() + i + "/" + (parseInt(new Date(start_date.value).getMonth()) + 1) + "/2023";
             document.querySelector("#day-" + i + "-row").classList.remove("opacity-0");
-            let current_day = document.createElement("p");
+            const current_day = document.createElement("p");
             current_day.innerHTML = day;
-            let current_date = document.createElement("b");
+            const current_date = document.createElement("b");
             current_date.innerHTML = date;
 
             document.querySelector("#day-" + i).innerHTML = "";
@@ -203,11 +204,11 @@ function show_reserved_days(total_time, start_date) {
 
 }
 function show_calendar() {
-    let calendar = document.querySelector("#show-calendar-btn");
-    let time_table = document.querySelector("#time-table");
+    const calendar = document.querySelector("#show-calendar-btn");
+    const time_table = document.querySelector("#time-table");
 
     calendar.addEventListener("click", (e) => {
-        let btn_text = document.querySelector("#show-calendar-btn p");
+        const btn_text = document.querySelector("#show-calendar-btn p");
         const is_open = e.target.getAttribute("opened");
         if (is_open == "true") {
             time_table.classList.add("opacity-0");
@@ -222,10 +223,10 @@ function show_calendar() {
 }
 
 function step_2_hide_pause_day() {
-    let checkboxes = document.querySelectorAll("#time-table input[type=\"checkbox\"]");
+    const checkboxes = document.querySelectorAll("#time-table input[type=\"checkbox\"]");
     checkboxes.forEach((elem) => {
         elem.addEventListener("change", (e) => {
-            let mainBox = e.target.parentNode.parentNode.parentNode;
+            const mainBox = e.target.parentNode.parentNode.parentNode;
             if (mainBox.classList.contains("line-through") ){
                 mainBox.classList.remove("line-through");
                 mainBox.querySelectorAll("input").forEach((input) => {
@@ -245,16 +246,16 @@ function step_2_hide_pause_day() {
 }
 
 function form_2_calendar_autofill() {
-    let start_time = document.querySelector("input[name=\"start-hour\"]");
-    let end_time = document.querySelector("input[name=\"end-hour\"]");
+    const start_time = document.querySelector("input[name=\"start-hour\"]");
+    const end_time = document.querySelector("input[name=\"end-hour\"]");
 
     start_time.value = "08:00";
     end_time.value = "18:00";
 
     let i = 0;
-    start_times = [];
-    end_times = [];
-    participants = [];
+    const start_times = [];
+    const end_times = [];
+    const participants = [];
     while (i < 5) {
         start_times.push(document.querySelector("input[name=\"day-" + i + "-start-time\"]"));
         end_times.push(document.querySelector("input[name=\"day-" + i + "-end-time\"]"));
@@ -263,32 +264,32 @@ function form_2_calendar_autofill() {
     }
 
     start_time.addEventListener("change", (e) => {
-        for (inputs of start_times) {
+        for (const inputs of start_times) {
             inputs.value = e.target.value
         }
     });
 
     end_time.addEventListener("change", (e) => {
-        for (inputs of end_times) {
+        for (const inputs of end_times) {
             inputs.value = e.target.value
         }
     });
 
     document.querySelector("#participant").addEventListener("change", (e) => {
-        for (participant of participants) {
+        for (const participant of participants) {
             participant.value = e.target.value;
         }
     });
 }
 
 function configuration_anim() {
-    for (let input of document.querySelectorAll("input[name=\"configuration\"]")) {
+    for (const input of document.querySelectorAll("input[name=\"configuration\"]")) {
         input.addEventListener("change", (e) => {
             document.querySelectorAll("input[name=\"configuration\"]").forEach((elem) => {
                 elem.parentNode.children[0].classList.remove("bg-focus-blue");
                 elem.parentNode.children[0].classList.remove("text-white");
             });
-            let label = e.target.parentNode.children[0];
+            const label = e.target.parentNode.children[0];
             label.classList.add("bg-focus-blue");
             label.classList.add("text-white");
         });
@@ -301,18 +302,18 @@ function add_anim() {
     let anim_cnt = 2;
     document.querySelector("#add-animateur").addEventListener("click", (e) => {
         e.preventDefault();
-        first_anim = document.querySelectorAll("#anim-1")[0];  
-        anim = first_anim.cloneNode(false);
+        const first_anim = document.querySelectorAll("#anim-1")[0];  
+        const anim = first_anim.cloneNode(false);
         anim.value = "";
         anim.name = "anim-"+anim_cnt;
         anim.id = "anim-"+anim_cnt;
         anim.val = "";
-        del_anim = document.querySelector("#del-btn").cloneNode(false);
+        const del_anim = document.querySelector("#del-btn").cloneNode(false);
         del_anim.className = "bg-primary rounded-md text-white m-[0.15em_0.25em_0.25em_0.25em] w-[2.5rem] h-[2.5rem] duration-150 hover:bg-secondary pt-1";
         del_anim.attributes["anim"] = anim_cnt;
         del_anim.id = "del-anim-"+anim_cnt;
         del_anim.innerHTML="-";
-        let divNode = first_anim.parentNode.cloneNode(false);
+        const divNode = first_anim.parentNode.cloneNode(false);
         divNode.append(anim);
         divNode.append(del_anim);
         first_anim.parentNode.parentNode.append(divNode);
