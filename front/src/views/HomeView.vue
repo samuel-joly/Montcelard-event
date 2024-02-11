726ba21435b1b6df0bb6bd6e9d06e6c284f7f6d3
samueljoly <samueljoly0@gmail.com>
2024-02-11 23:57:31 +0100

    Remove old dusty useless file

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { useLogin } from '@/stores/login'
+    import { useLogin } from '@/stores/login'
 import PanelNav from '@/components/PanelNav.vue'
 import { ref } from 'vue'
 import Login from '@/classes/Login'
@@ -10,25 +10,25 @@ const login: Login = new Login('Montcelard_user', 'Montcelard_user@gmail.com')
 </script>
 
 <template>
-  <div v-if="loginStore.jwt.length > 0">
-    <PanelNav />
-    <RouterView />
-  </div>
-  <div v-else="loginStore.jwt.length == 0">
-    <input v-model="login.name" />
-    <input v-model="password" />
-    <button @click="loginStore.logIn(login, password)">Login</button>
-  </div>
+    <div v-if="loginStore.jwt.length > 0">
+        <PanelNav />
+        <RouterView />
+    </div>
+    <div v-else="loginStore.jwt.length == 0">
+        <input v-model="login.name" />
+        <input v-model="password" />
+        <button @click="loginStore.logIn(login, password)">Login</button>
+    </div>
 </template>
 
 <style scoped>
 main {
-  display: flex;
-  background-color: #333300;
-  color: #ffffff;
+    display: flex;
+    background-color: #333300;
+    color: #ffffff;
 }
 
 div {
-  display: flex;
+    display: flex;
 }
 </style>
