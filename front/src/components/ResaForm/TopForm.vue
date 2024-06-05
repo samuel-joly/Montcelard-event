85193a8cbd5f207fa5bf9ce51f99069f61b19846
samueljoly <samueljoly0@gmail.com>
2024-06-05 12:36:50 +0200

    Feat[front]: dynamic select for date specific arrival/departure hour

--- front/src/components/ResaForm/TopForm.vue
+++ front/src/components/ResaForm/TopForm.vue
@@ -1,245 +1,309 @@
 <script lang="ts">
 import Icon from '@/components/Icon.vue'
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import { useResaFilter } from '@/stores/useResaFilter'
 import { useGridFilter } from '@/stores/useGridFilter'
 
 export default defineComponent({
-  components: {
-    Icon
-  },
-  setup() {
-    const filterStore = useResaFilter()
-    const gridStore = useGridFilter()
-    return {
-      filterStore,
-      gridStore
+    components: {
+        Icon
+    },
+    setup() {
+        const filterStore = useResaFilter()
+        const gridStore = useGridFilter()
+        const isSameHourAllWeek = ref<Boolean>(true)
+
+        const days = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi"];
+        return {
+            filterStore,
+            isFullWeek: isSameHourAllWeek,
+            gridStore,
+            days,
+        }
+    },
+    computed: {
+        getHostsName(): string[] | null {
+            if (this.gridStore.selected != null) {
+                return this.gridStore.selected.hostName.split(',')
+            }
+            return null
+        },
+
+        notSameHourDisplay() {
+            if (this.gridStore.selected != null) {
+                let startDay = this.gridStore.selected.startDate.getDay();
+                const endDay = this.gridStore.selected.endDate.getDay();
+                let days = [];
+                while (startDay <= endDay) {
+                    days.push(startDay);
+                    startDay++;
+                }
+                return days
+            } else {
+                throw Error("No selected reservation");
+            }
+        }
     }
-  },
-  computed: {
-    getHostsName(): string[] | null {
-      if (this.gridStore.selected != null) {
-        return this.gridStore.selected.hostName.split(',')
-      }
-      return null
-    }
-  }
 })
 </script>
 
 <template>
-  <div id="topInfo" v-if="gridStore.selected != null">
-    <input id="formationName" name="name" v-model="gridStore.selected.name" />
-    <div id="topBar">
-      <div class="flex-row just-between">
-        <span class="dateRow">
-          <label for="start_date">Du</label>
-          <input
-            name="start_date"
-            type="date"
-            :value="gridStore.selected.startDate.toISOString().split('T')[0]"
-            @input="
-              (event) => {
-                const newDate = (event.target as HTMLInputElement).valueAsDate
-                if (gridStore.selected != null && newDate != null) {
-                  gridStore.selected.startDate = newDate
-                }
-              }
-            "
-          />
-        </span>
-        <span class="dateRow">
-          <label for="paxNumber">pax</label>
-          <input id="paxNumber" type="number" name="guests" v-model="gridStore.selected.guests" />
-        </span>
-      </div>
-      <div class="flex-row just-between">
-        <span class="dateRow">
-          <label for="end_date">Au</label>
-          <input
-            name="end_date"
-            type="date"
-            :value="gridStore.selected.endDate.toISOString().split('T')[0]"
-            @input="
-              (event) => {
-                const newDate = (event.target as HTMLInputElement).valueAsDate
-                if (gridStore.selected != null && newDate != null) {
-                  gridStore.selected.endDate = newDate
-                }
-              }
-            "
-          />
-        </span>
-        <div class="just-between">
-          <span id="room-select dateRow">
-            <label for="room">Salle</label>
-            <select id="room" v-model="gridStore.selected.roomId">
-              <option value="1">Chine</option>
-              <option value="2">Cambodge</option>
-              <option value="3">Laos</option>
-              <option value="4">Jardin d'hiver</option>
-              <option value="5">Mali</option>
-              <option value="6">Myanmar</option>
-              <option value="7">Haiti</option>
-              <option value="8">Liban</option>
-              <option value="9">Madagascar</option>
-              <option value="10">Tadjikistan</option>
-              <option value="11">Bresil</option>
-              <option value="12">Orangerie</option>
-            </select>
-          </span>
+    <div id="topInfo" v-if="gridStore.selected != null">
+        <input id="formationName" name="name" v-model="gridStore.selected.name" />
+        <div id="topBar">
+            <div class="flex-row just-between">
+                <span class="dateRow">
+                    <label for="start_date">Du</label>
+                    <input name="start_date" type="date"
+                        :value="gridStore.selected.startDate.toISOString().split('T')[0]" @input="(event) => {
+                            const newDate = (event.target as HTMLInputElement).valueAsDate
+                            if (gridStore.selected != null && newDate != null) {
+                                gridStore.selected.startDate = newDate
+                            }
+                        }
+                            " />
+                </span>
+                <span class="dateRow">
+                    <label for="paxNumber">pax</label>
+                    <input id="paxNumber" type="number" name="guests" v-model="gridStore.selected.guests" />
+                </span>
+            </div>
+            <div class="flex-row just-between">
+                <span class="dateRow">
+                    <label for="end_date">Au</label>
+                    <input name="end_date" type="date" :value="gridStore.selected.endDate.toISOString().split('T')[0]"
+                        @input="(event) => {
+                            const newDate = (event.target as HTMLInputElement).valueAsDate
+                            if (gridStore.selected != null && newDate != null) {
+                                gridStore.selected.endDate = newDate
+                            }
+                        }
+                            " />
+                </span>
+                <div class="just-between">
+                    <span id="room-select dateRow">
+                        <label for="room">Salle</label>
+                        <select id="room" v-model="gridStore.selected.roomId">
+                            <option value="1">Chine</option>
+                            <option value="2">Cambodge</option>
+                            <option value="3">Laos</option>
+                            <option value="4">Jardin d'hiver</option>
+                            <option value="5">Mali</option>
+                            <option value="6">Myanmar</option>
+                            <option value="7">Haiti</option>
+                            <option value="8">Liban</option>
+                            <option value="9">Madagascar</option>
+                            <option value="10">Tadjikistan</option>
+                            <option value="11">Bresil</option>
+                            <option value="12">Orangerie</option>
+                        </select>
+                    </span>
+                </div>
+            </div>
         </div>
-      </div>
+        <div>
+
+            <div id="orgaFields">
+                <div class="flex-row just-between align-center">
+                    <div class="flex-col just-start align-center">
+                        <span class="flex-col">
+                            <label for="resp_dossier">Resp. dossier</label>
+                            <input name="respDossier" id="resp_dossier" v-model="gridStore.selected.orgaName" />
+                        </span>
+                        <span class="flex-col">
+                            <label for="resp_groupe">Resp. groupe</label>
+                            <input name="resp_groupe" id="resp_groupe" v-model="gridStore.selected.orgaName" />
+                        </span>
+                    </div>
+                    <div id="resaHour" class="flex-row just-around" style="width:100%;">
+                        <div class="flex-col align-center just-start">
+                            <div class="flex-row">
+                                <label for="start_hour" style="width:4em;">Arrivée</label>
+                                <input name="startHour" v-model="gridStore.selected.startHour" />
+                            </div>
+                            <div class="flex-row">
+                                <label for="end_hour" style="width:4em;">Départ</label>
+                                <input name="endHour" v-model="gridStore.selected.endHour" />
+                            </div>
+                        </div>
+                        <div class="flex-col just-end align-start" style="height:100%;">
+                            <label id="isHourFull" :class="[isFullWeek ? 'selectedFurniture' : '']" for="hourFullWeek">
+                                Tous les jours
+                            </label>
+                            <input class="hidden" type="checkbox" id="hourFullWeek" name="hourFullWeek"
+                                v-model="isFullWeek" />
+                            <select v-if="isFullWeek == false">
+                                <option v-for="day in notSameHourDisplay" :value="day">{{ days[day-1] }}</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="flex-col">
+                    <label for="host_name">Anim.</label>
+                    <input id="host_name" :v-model="gridStore.selected.hostName" :value="getHostsName" />
+                </div>
+            </div>
+        </div>
     </div>
-    <div>
-      <div id="orgaFields">
-        <span class="flex-col" style="width: 100%">
-          <label for="resp_dossier">Resp. dossier</label>
-          <input name="respDossier" id="resp_dossier" v-model="gridStore.selected.orgaName" />
-        </span>
-        <span class="flex-col" style="width: 100%">
-          <label for="resp_groupe">Resp. groupe</label>
-          <input name="resp_groupe" id="resp_groupe" v-model="gridStore.selected.orgaName" />
-        </span>
-        <span class="flex-col" style="width: 100%">
-          <label for="host_name">Anim.</label>
-          <input id="host_name" :v-model="gridStore.selected.hostName" :value="getHostsName" />
-        </span>
-      </div>
-    </div>
-  </div>
 </template>
 
 <style scoped>
 #orgaFields {
-  display: grid;
-  grid-template-columns: repeat(1, 1fr);
-  grid-template-rows: repeat(3, 1fr);
-  max-height: 90%;
+    display: grid;
 }
 
 #orgaFields span label {
-  font-size: 0.7em;
+    font-size: 0.7em;
 }
 
 #topInfo {
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  padding: 1rem;
-  font-size: 1.2rem;
-  border-bottom: 1px solid #ffffff;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    padding: 1rem;
+    font-size: 1.2rem;
+    border-bottom: 1px solid #ffffff;
 }
 
 #topInfo label {
-  font-size: 0.7em;
+    font-size: 0.7em;
 }
 
 #formationName:hover {
-  cursor: pointer;
+    cursor: pointer;
 }
 
 #formationName {
-  font-size: 1.3rem;
-  margin: 0px;
-  width: 100%;
+    font-size: 1.3rem;
+    margin: 0px;
+    width: 100%;
 }
 
 .dateInput {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
 }
 
 .dateInput span input {
-  margin: 0px;
-  padding: 0px !important;
-  font-size: 0.7em;
+    margin: 0px;
+    padding: 0px !important;
+    font-size: 0.7em;
 }
 
 #paxNumber {
-  width: 3rem;
+    width: 3rem;
 }
 
 #orgaInfo {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
 }
 
 #orgaInfo span {
-  display: flex;
-  padding-top: 0.1rem;
+    display: flex;
+    padding-top: 0.1rem;
 }
 
 .iconLabel {
-  display: flex;
-  width: fit-content;
-  justify-content: center;
-  align-items: center;
+    display: flex;
+    width: fit-content;
+    justify-content: center;
+    align-items: center;
 }
 
 #orgaInfo span label {
-  margin-right: 0.8rem;
-  font-size: 0.8em;
+    margin-right: 0.8rem;
+    font-size: 0.8em;
 }
 
 #hosts_name {
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  align-items: center;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: center;
 }
 
 #hosts_name input {
-  margin-top: 0.2em;
+    margin-top: 0.2em;
 }
 
 .addHostName,
 .hostNameBtn {
-  margin-left: 0.2em;
-  width: 22px;
-  height: 22px;
-  text-align: center;
-  font-size: 0.7em;
-  border-style: none;
-  border-radius: 4px;
-  border: 1px solid #eeeeee;
+    margin-left: 0.2em;
+    width: 22px;
+    height: 22px;
+    text-align: center;
+    font-size: 0.7em;
+    border-style: none;
+    border-radius: 4px;
+    border: 1px solid #eeeeee;
 }
 
 .hostName {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
 }
 
 .hostNameBox {
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
-  align-items: center;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
 }
 
 .hostNameLabel {
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
-  align-items: center;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
 }
 
 .dateRow {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: start;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: start;
 }
 
 .dateRow label {
-  width: 2em;
+    width: 2em;
 }
+
 #topBar {
-  margin-top: 0.25em;
+    margin-top: 0.25em;
+}
+
+
+#resaHour label {
+    font-size: 0.7em;
+    margin-top: 0.2em;
+}
+
+#resaHour input {
+    width: 4em;
+    margin-top: 0.2em;
+}
+
+.selectedFurniture {
+    background-color: #434f77;
+    color: white;
+    border: 1px solid black !important;
+}
+
+#isHourFull:hover {
+    cursor: pointer;
+}
+
+#isHourFull {
+    padding: 0.2em;
+    transition: 0.2s;
+    border: 1px solid white;
+    border-radius: 5px;
+    margin-bottom: 0.1em;
 }
 </style>
